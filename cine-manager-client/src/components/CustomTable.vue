0ebe70c4dd11583eee5a5a990f3070dc3538765a
Diogo Tavares <diogo.t0001@gmail.com>
2021-11-13 22:34:25 -0300

    Updates in custom table

---
+++ cine-manager-client/src/components/CustomTable.vue
@@ -0,0 +1,192 @@
+<template>
+  <v-container id="app">
+    <v-app id="inspire">
+      <v-data-table :headers="headers" :items="items" class="elevation-1">
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>{{ fields_p.title }}</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon class="mr-2" v-bind="attrs" v-on="on">
+                  mdi-plus-circle-outline
+                </v-icon>
+              </template>
+              <v-card dark>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="8" sm="4" md="4" v-for="(value, key) in editedItem" :key="key">
+                        <v-text-field 
+                          v-model="editedItem[key]"
+                          :label="getEditLabels(key)"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5"
+                  >Are you sure you want to delete this item?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
+    </v-app>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name:'CustomTable',
+  props: {
+    fields_p:{
+      title: { default: "Tabela" },
+      titleAddItem: { default: "Adicionar Item" },
+      titleEditItem: { default: "Editar Item" },
+      editLabels:{},
+      editedItem:{},
+      defaultItem: {},
+    },
+    headers_p:{default:[]},
+    items_p:{default:[]},
+  },
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [],
+    items: [],
+    editedIndex: -1,
+    editedItem: {},
+    defaultItem: {},
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? this.fields_p.titleAddItem : this.fields_p.titleEditItem;
+    },
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+    items(){
+
+    },
+    headers(){
+
+    }
+  },
+  created() {
+    console.log("Table >>> created() ")
+    this.initialize();
+  },
+  beforeUpdate(){
+    console.log("Table >>> beforeUpdate()")
+  },
+
+  methods: {
+    initialize() {
+      this.editedItem = {...this.fields_p.editedItem},
+      this.defaultItem = {...this.fields_p.defaultItem},
+      this.headers=[...this.headers_p];
+      this.items = [...this.items_p];
+      console.log(`${this.headers.length}  ${this.items.length}`)
+    },
+
+    editItem(item) {
+      this.editedIndex = this.items.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      console.log(`editedItem ${JSON.stringify(this.editedItem) }`)
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.items.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.items.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.items[this.editedIndex], this.editedItem);
+      } else {
+        this.items.push(this.editedItem);
+      }
+      this.close();
+    },
+    getEditLabels(label){
+      console.log(`getEditLabels ${label} -- ${this.fields_p.editLabels[label]}`)
+      return this.fields_p.editLabels[label];
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* .v-data-table,
+.v-icon{
+  color: rgb(38, 102, 158);
+} */
+</style>
